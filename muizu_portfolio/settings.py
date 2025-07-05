6d4d1bba0a1a7d1c883e65b514f3af6ac5e5419b
sapientergh1 <sapientergh@gmail.com>
2025-07-05 14:49:24 +0000

    Update settings.py

--- muizu_portfolio/settings.py
+++ muizu_portfolio/settings.py
@@ -138,8 +138,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Media files
+MEDIA_ROOT = BASE_DIR / 'staticfiles/media'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
